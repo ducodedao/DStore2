335ff09d1fb6013eba965b3f46cec20ddbc1d558
ducodedao <tongodu95@gmail.com>
2022-06-20 20:00:23 +0700

    Server Making Product API

--- server/src/app.js
+++ server/src/app.js
@@ -6,7 +6,9 @@ app.use(express.json())
 
 // Route
 const category = require('./routes/categoryRoute')
+const product = require('./routes/productRoute')
 
 app.use('/api/v1', category)
+app.use('/api/v1', product)
 
 module.exports = app
